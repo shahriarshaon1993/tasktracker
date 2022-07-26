0bd29d082ec88512451e0ff17bf703874372b324
Shahriar Shaon <shahriarshaon1993@gmail.com>
2022-07-26 23:30:54 +0600

    Implement Router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import './assets/main.css'
 import 'boxicons'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <h3>Verssion 1.0.0</h3>
+    <router-link to="/">Go back</router-link>
+</template>
+
+<script>
+    export default {
+        name: 'About'
+    }
+</script>
